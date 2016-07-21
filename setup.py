893483bf20991d3285eafd89e3f911e07b43768f
Chris van Marle <qistoph@gmail.com>
2016-07-21 16:35:38 +0200

    Update author and description

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(
     version='0.1dev',
     packages=['jsuntangler'],
     scripts=['bin/jsuntangle'],
+    author='Chris van Marle',
+    author_email='qistoph@gmail.com',
+    url='https://www.github.com/qistoph/jsuntangle',
     license='GPLv3',
-    long_description="none" #open('README').read()
+    long_description=open('README.md').read()
 )
