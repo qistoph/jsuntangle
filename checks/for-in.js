7517583ea7f6f05ef11603112fc898f820e241b1
Chris van Marle <qistoph@gmail.com>
2016-07-28 17:39:10 +0200

    Add for-in test

---
+++ checks/for-in.js
@@ -0,0 +1,4 @@
+var arr = [['one', 'two', 'three'], 'none']
+for(var i in arr[0]) {
+	console.log(i + ": " + arr[0][i]);
+}
---
+++ checks/out/for-in.js
@@ -0,0 +1,7 @@
+arr = [["one", "two", "three"], "none"];
+var arr;
+var i;
+
+for(var i in ["one", "two", "three"]) {
+  console["log"](i + ": " + ["one", "two", "three"][i]);
+}
